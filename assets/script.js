ff3fc53124b378d800722c76cfb453e1a7051c9d
Linn Erle <linnerlek@gmail.com>
2024-11-01 17:04:03 -0400

    updated select, project and rename node labels with input info

--- assets/script.js
+++ assets/script.js
@@ -30,28 +30,3 @@ window.dash_clientside = Object.assign({}, window.dash_clientside, {
         }
     }
 });
-
-document.addEventListener("DOMContentLoaded", function () {
-  const cy = window.cy;
-  if (!cy) return;
-
-  let tooltip = document.querySelector(".cy-tooltip");
-  if (!tooltip) {
-    tooltip = document.createElement("div");
-    tooltip.classList.add("cy-tooltip");
-    document.body.appendChild(tooltip);
-  }
-
-  cy.on("mouseover", "node", function (evt) {
-    const node = evt.target;
-    tooltip.innerHTML = node.data("tooltip");
-    tooltip.style.left = `${evt.renderedPosition.x}px`;
-    tooltip.style.top = `${evt.renderedPosition.y}px`;
-    tooltip.style.visibility = "visible";
-  });
-
-  cy.on("mouseout", "node", function (evt) {
-    tooltip.style.visibility = "hidden";
-  });
-});
-
